742afe4ed3b05629dd54b4621c9a69a3eb01a1d6
shoponpon <shoponprog@gmail.com>
2018-05-30 15:39:19 +0900

    add to generate Store.js

--- bin/index.js
+++ bin/index.js
@@ -22,15 +22,34 @@ function createAction(name){
   fs.writeFileSync(actionPath, `import ${name}ActionTypes from './${name}ActionTypes.js'\nconst ${name}Actions = {}\nexport default ${name}Actions\n`)
 }
 
+function createStore(name, dispathcerPath){
+  name = fixName(name)
+  const storePath = `${process.cwd()}/${name}Store.js`
+  fs.writeFileSync(storePath, `import { ReduceStore } from 'flux/utils'\nimport Dispatcher from '${dispathcerPath}'\n\nclass ${name}Store extends ReduceStore {\n  constructor() {\n    super(Dispatcher)\n  }\n\n  getInitialState() {\n    return {}\n  }\n\n  reduce(state, action) {\n    switch (action.type) {\n      default:\n        break\n    }\n    return Object.assign({}, state)\n  }\n}\n\nexport default new ${name}Store()`)
+}
+
 switch(type){
   case "action":
+    {
     if (process.argv.length < 4){
-      console.log(`flux-gen action [actionname]: actionname not found`)
+      console.log(`flux-gen action [name]: name not found`)
       break
     }
     const name = process.argv[3]
     createAction(name)
     break
+    }
+  case "store":
+    {
+    if (process.argv.length < 5){
+      console.log(`flux-gen store [name] [dispatcherPath]: name or dispatcherPath not found`)
+      break
+    }
+    const name = process.argv[3]
+    const dispatcherPath = process.argv[4]
+    createStore(name,dispatcherPath)
+    break
+    }
   default:
     console.log(`${type}: command not found`)
 }
