8fe3ad40479514fc0d2f809e17d98f50a6c8b0ba
shoponpon <shoponprog@gmail.com>
2018-06-01 00:27:34 +0900

    add import Dispatcher.js at generating Actions

--- bin/index.js
+++ bin/index.js
@@ -14,19 +14,20 @@ function fixName(str) {
   });
 }
 
-function createAction(name) {
+function createAction(name, dispatcherPath) {
+  dispatcherPath = dispatcherPath.slice(0,-3)
   name = fixName(name)
   const actionPath = `${process.cwd()}/${name}Actions.js`
   const actionTypePath = `${process.cwd()}/${name}ActionTypes.js`
   fs.writeFileSync(actionTypePath, `const ${name}ActionTypes = {}\nexport default ${name}ActionTypes\n`)
-  fs.writeFileSync(actionPath, `import ${name}ActionTypes from './${name}ActionTypes'\nconst ${name}Actions = {}\nexport default ${name}Actions\n`)
+  fs.writeFileSync(actionPath, `import ${name}ActionTypes from './${name}ActionTypes'\nimport Dispatcher from '${dispatcherPath}'\nconst ${name}Actions = {}\nexport default ${name}Actions\n`)
 }
 
-function createStore(name, dispathcerPath) {
-  dispathcerPath = dispathcerPath.slice(0,-3)
+function createStore(name, dispatcherPath) {
+  dispatcherPath = dispatcherPath.slice(0,-3)
   name = fixName(name)
   const storePath = `${process.cwd()}/${name}Store.js`
-  fs.writeFileSync(storePath, `import { ReduceStore } from 'flux/utils'\nimport Dispatcher from '${dispathcerPath}'\n\nclass ${name}Store extends ReduceStore {\n  constructor() {\n    super(Dispatcher)\n  }\n\n  getInitialState() {\n    return {}\n  }\n\n  reduce(state, action) {\n    switch (action.type) {\n      default:\n        break\n    }\n    return Object.assign({}, state)\n  }\n}\n\nexport default new ${name}Store()`)
+  fs.writeFileSync(storePath, `import { ReduceStore } from 'flux/utils'\nimport Dispatcher from '${dispatcherPath}'\n\nclass ${name}Store extends ReduceStore {\n  constructor() {\n    super(Dispatcher)\n  }\n\n  getInitialState() {\n    return {}\n  }\n\n  reduce(state, action) {\n    switch (action.type) {\n      default:\n        break\n    }\n    return Object.assign({}, state)\n  }\n}\n\nexport default new ${name}Store()`)
 }
 
 function createDispatcher() {
@@ -37,12 +38,13 @@ function createDispatcher() {
 switch (type) {
   case "action":
     {
-      if (process.argv.length < 4) {
-        console.log(`flux-gen action [name]: name not found`)
+      if (process.argv.length < 5) {
+        console.log(`flux-gen action [name] [dispatcherPath]: name or dispatcherPath not found`)
         break
       }
       const name = process.argv[3]
-      createAction(name)
+      const dispatcherPath = process.argv[4]
+      createAction(name, dispatcherPath)
       break
     }
   case "store":
